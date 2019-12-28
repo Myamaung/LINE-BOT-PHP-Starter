6e046bd8472d2403f06303e28e76eaa4158abe25
Asgard <59232445+Myamaung@users.noreply.github.com>
2019-12-29 01:24:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -36,9 +36,9 @@ echo $content;
     #ตัวอย่าง Message Type "Image"
     else if($message == "รูปน้องแมว"){
        
-        $image_url = "http://188.166.206.43/OsOZS2EVhI2ptYrsJ9g1u3rBmCkhsjbH/update/V8?value=0";
+        $image_url = "https://188.166.206.43/OsOZS2EVhI2ptYrsJ9g1u3rBmCkhsjbH/update/V8?value=0";
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        replyMsg($arrayHeader,$arrayPostData);
+       
     }
     #ตัวอย่าง Message Type "Location"
     else if($message == "พิกัดสยามพารากอน"){
